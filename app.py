d2f4373ee126706b96ea4399935c50b1b39f200d
Omar2127 <0002127@up.edu.mx>
2026-09-19 01:00:32 -0600

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import plotly.express as px
+import json
+import urllib.request
 from scipy.optimize import curve_fit
 
 st.set_page_config(layout="wide")
