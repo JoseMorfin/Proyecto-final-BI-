278ce4edbd7b81e7d8dce3cee96a4a76ca73ef3f
Omar2127 <0002127@up.edu.mx>
2026-09-19 01:01:12 -0600

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-import plotly.express as px
 import json
 import urllib.request
 from scipy.optimize import curve_fit
